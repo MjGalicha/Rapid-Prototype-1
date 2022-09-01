ccd1709722d7a3e54c096997164a4e53aaed6972
Mj Galicha <u1178025@ad.utah.edu>
2022-09-01 10:07:34 -0600

    Revert "feat(engineering): [RP1-17] Updated Snake Movement to use arow keys"
    This reverts commit c006564b6f994c3c1dc937b83b9bb0f4196a20ae.

--- Assets/Scripts/SnakeMovement.cs
+++ Assets/Scripts/SnakeMovement.cs
@@ -25,19 +25,19 @@ public class SnakeMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W) | Input.GetKeyDown(KeyCode.UpArrow))                         /* This nested loop gets player input and updates direction */
+        if(Input.GetKeyDown(KeyCode.W))                         /* This nested loop gets player input and updates direction */
         {
             direction = Vector2.up;
         }
-        else if (Input.GetKeyDown(KeyCode.S) | Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.S))
         {
             direction = Vector2.down;
         }
-        else if (Input.GetKeyDown(KeyCode.A) | Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (Input.GetKeyDown(KeyCode.A))
         {
             direction = Vector2.left;
         }
-        else if (Input.GetKeyDown(KeyCode.D) | Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.D))
         {
             direction = Vector2.right;
         }
