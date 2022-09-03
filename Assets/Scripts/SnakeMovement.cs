b85875dcd180959e09048b402578419d1b2aee39
Zephyrus20 <u1429413@umail.utah.edu>
2022-09-03 11:59:30 -0600

    Player turning on himself fixed

--- Assets/Scripts/SnakeMovement.cs
+++ Assets/Scripts/SnakeMovement.cs
@@ -9,6 +9,10 @@ public class SnakeMovement : MonoBehaviour
     public Transform segmentPrefab;
     List<Transform> segments;
     public bool CanMove = true;
+    bool CanMoveLeft = true;
+    bool CanMoveRight = true;
+    bool CanMoveBack = true;
+    bool CanMoveFront = true;
 
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
@@ -30,21 +34,49 @@ public class SnakeMovement : MonoBehaviour
     {
         if(CanMove)
         {
+<<<<<<< Updated upstream
             if (Input.GetKeyDown(KeyCode.W) | Input.GetKeyDown(KeyCode.UpArrow))                         /* This nested loop gets player input and updates direction */
+=======
+            if (Input.GetKeyDown(KeyCode.W) && CanMoveFront == true)                         /* This nested loop gets player input and updates direction */
+>>>>>>> Stashed changes
             {
                 direction = Vector2.up;
+                CanMoveBack = false;
+                CanMoveRight = true;
+                CanMoveLeft = true;
             }
+<<<<<<< Updated upstream
             else if (Input.GetKeyDown(KeyCode.S) | Input.GetKeyDown(KeyCode.DownArrow))
+=======
+            else if (Input.GetKeyDown(KeyCode.S) && CanMoveBack == true)
+>>>>>>> Stashed changes
             {
                 direction = Vector2.down;
+                CanMoveFront = false;
+                CanMoveRight = true;
+                CanMoveLeft = true;
             }
+<<<<<<< Updated upstream
             else if (Input.GetKeyDown(KeyCode.A) | Input.GetKeyDown(KeyCode.LeftArrow))
+=======
+            else if (Input.GetKeyDown(KeyCode.A) && CanMoveLeft == true)
+>>>>>>> Stashed changes
             {
                 direction = Vector2.left;
+                CanMoveRight = false;
+                CanMoveFront = true;
+                CanMoveBack = true;
             }
+<<<<<<< Updated upstream
             else if (Input.GetKeyDown(KeyCode.D) | Input.GetKeyDown(KeyCode.RightArrow))
+=======
+            else if (Input.GetKeyDown(KeyCode.D) && CanMoveRight == true)
+>>>>>>> Stashed changes
             {
                 direction = Vector2.right;
+                CanMoveLeft = false;
+                CanMoveFront = true;
+                CanMoveBack = true;
             }
         }
     }
